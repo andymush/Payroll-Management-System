04c754e9c13ef8acd66fdde2477871c361472b88
andymush <andymush9@gmail.com>
2024-01-04 12:43:28 +0300

    breeze & vue layouts added

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,20 @@
 import './bootstrap';
+
+// import Alpine from 'alpinejs';
+
+// window.Alpine = Alpine;
+
+// Alpine.start();
+
+
+import { createApp } from 'vue';
+import ComponentA from './components/ComponentA.vue';
+import ComponentB from './components/ComponentB.vue';
+
+const app = createApp({});
+
+app
+  .component('ComponentA', ComponentA)
+  .component('ComponentB', ComponentB);
+
+app.mount("#app");
---
+++ resources/js/components/ComponentB.vue
@@ -0,0 +1,21 @@
+<!-- MyComponent.vue -->
+<template>
+    <div>
+      <h1>{{ message }}</h1>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        message: 'Hello, Vue SFC!',
+      };
+    },
+  };
+  </script>
+  
+  <style scoped>
+  /* Your styles here */
+  </style>
+  
