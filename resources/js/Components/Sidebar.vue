dc84078c50ee6e34e7e5cdbcc40db8b3cf4ed876
andymush <andymush9@gmail.com>
2024-01-05 12:35:22 +0300

    fetch data using vue

---
+++ resources/js/Components/Sidebar.vue
@@ -0,0 +1,3 @@
+<template>
+    <p>sidewways</p>
+</template>
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use App\Http\Controllers\EmployeesController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,6 +16,7 @@ use Inertia\Inertia;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::resource("employees", EmployeesController::class);
 
 Route::get('/', function () {
     return Inertia::render('Welcome', [
@@ -28,6 +30,7 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
