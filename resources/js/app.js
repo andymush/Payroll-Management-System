c664f4cb52ddc2eb2deac89713453d7bbc00cc0a
andymush <andymush9@gmail.com>
2024-01-16 12:29:41 +0300

    template import errors

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,7 @@
 import './bootstrap';
 import '../css/app.css';
+import PerfectScrollbar from 'vue3-perfect-scrollbar';
+
 
 import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/vue3';
@@ -15,6 +17,7 @@ createInertiaApp({
         return createApp({ render: () => h(App, props) })
             .use(plugin)
             .use(ZiggyVue)
+            .use(PerfectScrollbar)
             .mount(el);
     },
     progress: {
